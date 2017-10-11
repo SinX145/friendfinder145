1ce0f078ac69fbc313051e117116b65ad69f4c40
Dev Patel <sinx145@gmail.com>
2017-10-10 20:58:02 -0400

    added stuff

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [ {
+	"name" : "James Bond",
+	"photo" : "https://media.gq.com/photos/5633ef2edf121979786a03d0/3:2/w_880/flower-bond-3x2_1024.jpg",
+	"scores" : [5,1,2,3,1,4,2,1,3,4]
+}, {
+	"name" : "DeadPool",
+	"photo" : "https://m.media-amazon.com/images/S/aplus-media/vc/dbc1c785-96ff-4e48-97bd-9671fb97648e._SL300__.png",
+	"scores" : [1,5,1,2,1,2,1,2,3,5]
+}, {
+	"name" : "God",
+	"photo" : "http://www.telegraph.co.uk/content/dam/video/MF-xlarge.jpg",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}, {
+	"name" : "Tony Stark",
+	"photo" : "https://vignette4.wikia.nocookie.net/ironman/images/e/ee/Photo%28696%29.jpg/revision/latest?cb=20130922104708",
+	"scores" : [3,1,3,3,2,3,1,2,3,4]
+}];
